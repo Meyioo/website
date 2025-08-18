c08423ff49f359eb95687c7807fcd2cc2949798a
Meyioo <tim.meyerhoff@outlook.de>
2025-08-18 21:09:05 +0200

    style picture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [FooterComponent, ProfilePictureComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
