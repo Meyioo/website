a6a2a9fcdd546ad0785de88e647f17d9ac4be5fb
Meyioo <tim.meyerhoff@outlook.de>
2025-08-19 11:28:18 +0200

    Refactor app layout: replace profile picture component with an image, add navigation bar, and update styles to use Tailwind CSS with @use. Remove unused profile picture component files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
-import { ProfilePictureComponent } from './profile-picture/profile-picture.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent, ProfilePictureComponent],
+  imports: [FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
